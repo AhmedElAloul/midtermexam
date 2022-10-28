45394a694b7f7573f8bded95fb432da0f3764055
Ahmed Al <ahmedaloul22@gmail.com>
2022-10-28 16:28:26 -0400

    Dot env added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let app = require("./server/config/app");
 let debug = require("debug")("comp308-w2019-midterm:server");
 let http = require("http");
+const dotenv = require("dotenv")
+dotenv.config()
 
 
 /**
